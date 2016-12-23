11a2652f80743780b21781747ceb44e651f960e1
Ning Ning Sun <nsun200@live.com>
2016-12-23 18:03:43 -0500

    Initial commit

---
+++ BatchWAVtoMP3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace BatchWAVtoMP3
+{
+    class Program
+    {
+        public const string fileExtension = ".mp3";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your music directory's path: ");
+            string path = Console.ReadLine();
+            string[] files = Directory.GetFiles(path);
+            foreach (string file in files)
+            {
+                int dotIndex = file.IndexOf('.');
+                string newFile = file.Substring(0, dotIndex);
+                newFile += fileExtension;
+                Process process = new Process();
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.FileName = "lame.exe";
+                process.StartInfo.Arguments = string.Format("-b 128 \"{0}\" \"{1}\"", file, newFile);
+                process.Start();
+                process.WaitForExit();
+                File.Delete(file);
+            }
+        }
+    }
+}
